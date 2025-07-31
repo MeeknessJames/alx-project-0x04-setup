7c1ea9c76de48a1d45972282028cf44a3a76a9de
Meekness James <meeknessjames1@gmail.com>
2025-08-01 00:35:59 +0100

    fix: resolve ContextAPI naming and layout import errors

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { CountProvider } from '../context/CountContext'; // Corrected import
+import Layout from '../components/layouts/Layout'; // Added Layout import
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
 }
+
+export default MyApp;
