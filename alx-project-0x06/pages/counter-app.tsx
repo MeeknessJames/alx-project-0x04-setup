163275bb7699c6497ac8163c94aa4e6659dfca61
Meekness James <meeknessjames1@gmail.com>
2025-08-01 02:43:03 +0100

    fix: add Redux hooks to counter-app.tsx

--- alx-project-0x06/pages/counter-app.tsx
+++ alx-project-0x06/pages/counter-app.tsx
@@ -1,9 +1,18 @@
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '../store/store';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useAppDispatch } from '../store/hooks'
+import type { RootState, AppDispatch } from '../store/store'
 
 const CounterApp = () => {
-  const dispatch: AppDispatch = useDispatch();
-  return null;
-};
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch: AppDispatch = useAppDispatch()
 
-export default CounterApp;
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      {/* Add dispatch actions here */}
+    </div>
+  )
+}
+
+export default CounterApp
